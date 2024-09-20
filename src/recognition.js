77d5c2b7b44b6763b44e4bec8eca851640718834
ballgoesvroomvroom <chongjayden07@gmail.com>
2024-09-21 02:19:20 +0800

    speech service failure robust check

--- src/recognition.js
+++ src/recognition.js
@@ -23,12 +23,17 @@ const recognitionModel = (socket, lang, closeFn, res) => {
 		}
 		console.log("deepgram: connected!!", res);
 
+		let emptyTranscriptCount = 0
+		let transcriptContentPrior; // store the latest non-empty transcribed content here
 		deepgram.addListener(LiveTranscriptionEvents.Transcript, (data) => {
 			console.log("deepgram: transcript received\n\t", data.type, data.speech_final, data.channel.alternatives[0].transcript);
 
 			if (data.speech_final) {
-				// End of speech
+				// end of speech
 				if (data.channel.alternatives[0].transcript.length >= 1) {
+					emptyTranscriptCount = 0
+					transcriptContentPrior = null // reset content
+
 					socket.emit("transcription", {
 						type: "end",
 						content: data.channel.alternatives[0].transcript,
@@ -36,9 +41,30 @@ const recognitionModel = (socket, lang, closeFn, res) => {
 					});
 
 					closeFn() // call close function
+				} else {
+					emptyTranscriptCount += 1
+					if (emptyTranscriptCount >= 2) {
+						// 2 consecutive empty transcripts
+						if (transcriptContentPrior) {
+							// guaranteed to have content (where transcriptContentPrior.length >= 1); send end event
+							socket.emit("transcription", {
+								type: "end",
+								content: data.channel.alternatives[0].transcript,
+								duration: data.duration
+							})
+						} else {
+							// no prior content to go with; have client retry once current deepgram instance is closed
+							socket.emit("transcription-failure")
+						}
+					}
+
+					closeFn()
 				}
 			} else {
-				// Interim results
+				// interim results
+				emptyTranscriptCount = 0
+				transcriptContentPrior = data.channel.alternatives[0].transcript
+
 				socket.emit("transcription", {
 					type: "interim",
 					content: data.channel.alternatives[0].transcript,
